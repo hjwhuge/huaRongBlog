53e108acdd7d3dfee61779a8e7d611b2b06a222e
USER09 <USER09@gzoffice.com>
2019-06-10 10:48:25 +0800

    目录结构

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    // 部署的时候需要用到
+    base:'/blog/',
+    // dest: './docs/.vuepress/dist',
+    title: '我的博客',
+    description: '我的博客',
+    // theme: '@vuepress/blog',
+    // themeConfig: {
+    //     // 这是嵌套标题链接，自动显示当前激活（导航）页面标题的链接，即显示深度（h1- h6的深度）
+    //     sidebarDepth: 1,
+    //     // text为导航栏显示文字，link为路径，即文件夹名字，注意不要丢了名字前后的'/'
+    //     nav: [
+    //         { text: 'Home', link: '/' },
+    //         { text: 'Guide', link: '/guide/' },
+    //         { text: 'About', link: '/about/' },
+    //         { text: 'Study', link: '/study/' },
+    //         { text: 'Github', link: 'https://github.com/hjwhuge?tab=repositories' },
+    //     ],
+    //     // 侧边栏配置,侧边栏组，不同（导航）页面对应不同的侧边栏
+    //     // 以对象形式配置，前边的key为nav处的路径,后边提供一个数组作为此侧边栏中的子标题
+    //     sidebar: {
+    //         '/About/': [
+    //             // ''空字符串代表主页，显示README.md中的内容
+    //             '',
+    //             ['201709', '201709'],//使用数组为侧边栏起别名，前边是md名称，后边是链接显示的文字
+    //             ['201710', '201710'],
+    //         ],
+    //         '/study/': [
+    //             '',
+    //             ['axios', '1.axios'],
+    //             ['document', '2.document'],
+    //         ]
+    //     },
+
+    // },
+}
