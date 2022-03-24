923916ec6b2d9b422c34e5bfbc9b5dced42373dd
hjwhuge <hujiawei_h@163.com>
2022-03-24 10:37:35 +0800

    feat: 部署脚本和打包路径修改

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/hjwhuge/huaRongBlog.git master:gh-pages
+# git push -f https://github.com/hjwhuge/huaRongBlog.git master:gh-pages
+git push -f git@github.com:hjwhuge/huaRongBlog.git master:gh-pages
 
 cd -
